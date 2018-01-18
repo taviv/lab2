cb8695de5721467421fc943172b07e475cfc6e25
Vivian Ta <vivian.vta@gmail.com>
2018-01-17 21:17:22 -0800

    added JavaScript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -5,14 +5,24 @@ $(document).ready(function() {
 	initializePage();
 })
 
+$("a.thumbnail").click(projectClick);
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript works");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#F1948A");
 }
